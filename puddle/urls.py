637e6029de4b5a6c6ed7a7166e61019a0b8341a4
Zhen Duan <zhen@arkade.com.au>
2023-04-25 22:25:19 +0800

    base.html title, create contact page

--- puddle/urls.py
+++ puddle/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
     path('',index,name="index"),
+    path('contact/', contact,name='contact'),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
