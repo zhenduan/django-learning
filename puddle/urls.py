3642953d783d82febaf9e6e465120b240b28d59f
Zhen Duan <zhen@arkade.com.au>
2023-04-25 10:38:39 +0800

    set up tailwindcss in django

--- puddle/urls.py
+++ puddle/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 from core.views import index
 
 urlpatterns = [
     path('',index,name="index"),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
